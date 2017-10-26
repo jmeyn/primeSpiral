968d26dae74ea75e78d1b7af95ed3eb6421c8cc1
Julian Meyn <Ic3Venom@gmail.com>
2017-10-26 07:44:07 -0700

    Update main.go

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ type coord struct {
 }
 
 func isPrime(guess uint32) bool {
-	if guess % 2 == 0 {
+	if guess%2 == 0 {
 		return false
 	}
-	
-	for i := uint32(3); i < uint32(math.Sqrt(float64(guess))) +1; i += 2 {
-		if guess % i == 0 {
+
+	for i := uint32(3); i < uint32(math.Sqrt(float64(guess)))+1; i += 2 {
+		if guess%i == 0 {
 			return false
 		}
 	}
@@ -25,50 +25,50 @@ func isPrime(guess uint32) bool {
 
 func main() {
 	//primes := []int{2}
-	grid := make([]coord, 1000)
-	//var table [10][10]int
+	grid := make([]coord, 400)
+	var table [20][20]int
 
-	max := 1000
+	max := 400
 	x, y := 0, 0
 	direction := true
 
 	grid[0] = coord{x, y, 2}
 
-	for i, lim := 0, 1; i < max -1; lim++ {
+	for i, lim := 0, 1; i < max-1; lim++ {
 		if direction == true {
-			for k := 0; k < lim && i < max -1; k++ {
+			for k := 0; k < lim && i < max-1; k++ {
 				x++
 				i++
-				grid[i] = coord{x, y, i +2}
+				grid[i] = coord{x, y, i + 2}
 			}
-			for k := 0; k < lim && i < max -1; k++ {
+			for k := 0; k < lim && i < max-1; k++ {
 				y++
 				i++
-				grid[i] = coord{x, y, i +2}
+				grid[i] = coord{x, y, i + 2}
 			}
 		} else {
-			for k := 0; k < lim && i < max -1; k++ {
+			for k := 0; k < lim && i < max-1; k++ {
 				x--
 				i++
-				grid[i] = coord{x, y, i +2}
+				grid[i] = coord{x, y, i + 2}
 			}
-			for k := 0; k < lim && i < max -1; k++ {
+			for k := 0; k < lim && i < max-1; k++ {
 				y--
 				i++
-				grid[i] = coord{x, y, i +2}
+				grid[i] = coord{x, y, i + 2}
 			}
 		}
 		direction = !direction
 	}
 
-	/*for i := 0; i < len(grid); i++ {
+	for i := 0; i < len(grid); i++ {
 		table[9 + grid[i].X][9 + grid[i].Y] = grid[i].val
-	}*/
+	}
 
-	for i := 0; i < max; i++ {
-		fmt.Printf("{%3d,%3d,%4d}", grid[i].X, grid[i].Y, grid[i].val)
-		if i % 10 == 0 {
-			fmt.Println()
+	for i := 0; i < 401; i++ {
+		for j := 0; j < 20; j++ {
+			fmt.Printf("%4d ", table[i][j])
 		}
+		fmt.Println()
 	}
 }
