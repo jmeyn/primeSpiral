d4480338dcc9cf4003648a1935e0381023b408a9
Julian Meyn <Ic3Venom@gmail.com>
2017-10-29 19:25:53 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
+	"image"
+	"image/png"
+	"os"
+	"image/color"
 )
 
 type coord struct {
@@ -10,14 +14,21 @@ type coord struct {
 }
 
 func isPrime(guess uint32) bool {
-	if guess%2 == 0 {
+	if guess % 2 == 0 {
+		if guess == 2 {
+			return true
+		}
 		return false
 	}
 
-	for i := uint32(3); i < uint32(math.Sqrt(float64(guess)))+1; i += 2 {
-		if guess%i == 0 {
+	for i := uint32(3); i < uint32(math.Sqrt(float64(guess))) + 1; i += 2 {
+		if guess % i == 0 {
 			return false
 		}
+	}
+
+	if guess == 1 {
+		return false
 	}
 
 	return true
@@ -25,50 +36,64 @@ func isPrime(guess uint32) bool {
 
 func main() {
 	//primes := []int{2}
-	grid := make([]coord, 400)
-	var table [20][20]int
+	const SIZE, LEN = 1234321, 1111
+	grid := make([]coord, SIZE)
+	var table [LEN][LEN]int
 
-	max := 400
 	x, y := 0, 0
 	direction := true
+	img := image.NewRGBA(image.Rect(0, 0, LEN, LEN))
 
-	grid[0] = coord{x, y, 2}
+	grid[0] = coord{x, y, 1}
 
-	for i, lim := 0, 1; i < max-1; lim++ {
+	for i, lim := 0, 1; i < SIZE-1; lim++ {
 		if direction == true {
-			for k := 0; k < lim && i < max-1; k++ {
+			for k := 0; k < lim && i < SIZE-1; k++ {
 				x++
 				i++
-				grid[i] = coord{x, y, i + 2}
+				grid[i] = coord{x, y, i + 1}
 			}
-			for k := 0; k < lim && i < max-1; k++ {
+			for k := 0; k < lim && i < SIZE-1; k++ {
 				y++
 				i++
-				grid[i] = coord{x, y, i + 2}
+				grid[i] = coord{x, y, i + 1}
 			}
 		} else {
-			for k := 0; k < lim && i < max-1; k++ {
+			for k := 0; k < lim && i < SIZE-1; k++ {
 				x--
 				i++
-				grid[i] = coord{x, y, i + 2}
+				grid[i] = coord{x, y, i + 1}
 			}
-			for k := 0; k < lim && i < max-1; k++ {
+			for k := 0; k < lim && i < SIZE-1; k++ {
 				y--
 				i++
-				grid[i] = coord{x, y, i + 2}
+				grid[i] = coord{x, y, i + 1}
 			}
 		}
 		direction = !direction
 	}
 
+	offset := int(LEN/2)
 	for i := 0; i < len(grid); i++ {
-		table[9 + grid[i].X][9 + grid[i].Y] = grid[i].val
+		table[offset - grid[i].Y][offset + grid[i].X] = grid[i].val
+		if isPrime(uint32(grid[i].val)) == true {
+			if grid[i].val == 2 {
+				img.Set(offset + grid[i].X, offset - grid[i].Y, color.Gray16{0})
+			} else {
+				img.Set(offset + grid[i].X, offset - grid[i].Y, color.RGBA{0, 255, 0, 255})
+			}
+		} else {
+			img.Set(offset + grid[i].X, offset - grid[i].Y, color.Gray16{0xffff})
+		}
 	}
 
-	for i := 0; i < 401; i++ {
-		for j := 0; j < 20; j++ {
-			fmt.Printf("%4d ", table[i][j])
-		}
-		fmt.Println()
+	f, err := os.Create("draw.png")
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	
+	defer f.Close()
+	png.Encode(f, img)
 }
