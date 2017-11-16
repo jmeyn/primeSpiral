26445aa9cc2c833e992ff47aaeeb82be9f280864
Julian Meyn <Ic3Venom@gmail.com>
2017-11-16 07:41:43 -0800

    Update diagonal.go

--- diagonal.go
+++ diagonal.go
@@ -9,17 +9,89 @@ import (
 	"os"
 )
 
-type coord struct {
-	X, Y, val int
+func isPrime(guess int) bool {
+	if guess%2 == 0 {
+		if guess == 2 {
+			return true
+		}
+
+		return false
+	}
+
+	for i := 3; i < int(math.Sqrt(float64(guess))+1); i += 2 {
+		if i == 5 {
+			continue
+		}
+		if guess%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
-	const SIZE, LEN = 121, 11
-
+	const SIZE, LEN = 1000000, 1000
+	/*red := 255
+	green := 0
+	blue := 0*/
+	n := 2
 	img := image.NewRGBA(image.Rect(0, 0, LEN, LEN))
 
-	for i, lim := 0, 1; i < SIZE-1; lim++ {
+	for i := 0; i < LEN-1; i++ {
+		for j, k := 0, i; k >= 0; {
+			if isPrime(n) == true {
+				img.Set(j, k, color.RGBA{0, 0, 0, 255})
+			} else {
+				img.Set(j, k, color.RGBA{255, 0, 255, 255})
+			}
 
+			/*
+				if red == 255 && blue == 0 && green == 0 {
+					blue = 0
+					red = 0
+					green += 255
+				} else if red == 0 && blue == 0 && green == 255 {
+					blue += 255
+					red = 0
+					green = 0
+				} else {
+					blue = 0
+					red += 255
+					green = 0
+				}*/
+
+			j++
+			k--
+			n++
+		}
+	}
+	for i := 0; i < LEN; i++ {
+		for j, k := i, LEN-1; k >= i; {
+			if isPrime(n) == true {
+				img.Set(j, k, color.RGBA{0, 0, 0, 255})
+			} else {
+				img.Set(j, k, color.RGBA{255, 0, 255, 255})
+			}
+
+			/*if red == 255 && blue == 0 && green == 0 {
+				blue = 0
+				red = 0
+				green += 255
+			} else if red == 0 && blue == 0 && green == 255 {
+				blue += 255
+				red = 0
+				green = 0
+			} else {
+				blue = 0
+				red += 255
+				green = 0
+			}*/
+
+			j++
+			k--
+			n++
+		}
 	}
 
 	f, err := os.Create("draw.png")
